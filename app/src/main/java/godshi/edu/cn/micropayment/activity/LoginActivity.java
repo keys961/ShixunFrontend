a76a2fc602fff165bc0b809c8951e5712341a352
keys961 <keys961@hotmail.com>
2018-07-12 15:32:59 +0800

    Initial commit

---
+++ app/src/main/java/godshi/edu/cn/micropayment/activity/LoginActivity.java
@@ -0,0 +1,18 @@
+package godshi.edu.cn.micropayment.activity;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import godshi.edu.cn.micropayment.R;
+
+public class LoginActivity extends Activity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+}
